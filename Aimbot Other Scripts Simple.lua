7794b681879d40e7c1fb837c0f046bff85644a57
Dxrk3867Bot <Dxrkwareservice@gmail.com>
2025-03-13 17:25:01 +0100

    Create Aimbot Other Scripts Simple.lua

---
+++ Aimbot Other Scripts Simple.lua
@@ -0,0 +1,22 @@
+_G.Theme = "Mocha"
+
+--Themes: Light, Dark, Red, Mocha, Aqua and Jester
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/Rivals.lua"))()
+
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Dxrk38677/ssssssssss/refs/heads/main/Telport%20behind%20(J).lua'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Blissful4992/ESPs/refs/heads/main/SkeletonESP.lua'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Dxrk38677/ssssssssss/refs/heads/main/Fly.lua'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Dxrk38677/ssssssssss/refs/heads/main/teleport%20on%20top%20(K).lua'))()
+end)
