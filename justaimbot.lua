b39f14402a4b5776a97fe2b99dd0fbd23eccbec1
Dxrk3867Bot <Dxrkwareservice@gmail.com>
2025-03-13 17:25:14 +0100

    Create justaimbot.lua

---
+++ justaimbot.lua
@@ -0,0 +1,6 @@
+_G.Theme = "Mocha"
+
+--Themes: Light, Dark, Red, Mocha, Aqua and Jester
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/Rivals.lua"))()
+
