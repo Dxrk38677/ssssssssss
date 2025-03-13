0f9b270f5266b202e60a1037b9fd9f6922f81264
Dxrk3867Bot <Dxrkwareservice@gmail.com>
2025-03-13 17:25:31 +0100

    Create simplescripts.lua

---
+++ simplescripts.lua
@@ -0,0 +1,16 @@
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Dxrk38677/ssssssssss/refs/heads/main/Telport%20behind%20(J).lua'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Blissful4992/ESPs/refs/heads/main/SkeletonESP.lua'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Dxrk38677/ssssssssss/refs/heads/main/Fly.lua'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Dxrk38677/ssssssssss/refs/heads/main/teleport%20on%20top%20(K).lua'))()
+end)
